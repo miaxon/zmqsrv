eec5f2178c91d8357986446f918fb95514811b24
eaxme <alexcon314@gmail.com>
2017-05-10 23:27:14 +0300

    init commit

---
+++ zmqsrv/ZMQService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zmqsrv
+{
+    public partial class ZMQService : ServiceBase
+    {
+        public ZMQService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
